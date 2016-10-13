92a856d50205375564be5d5026de0c293a81fc47
ymgn <gonyamagon@gmail.com>
2016-10-13 19:19:09 +0900

    ja5

--- api.py
+++ api.py
@@ -30,6 +30,7 @@ def slide():
     data_list = [] # 全ページのデータを集める配列
     # select = Select(driver.find_element_by_id('slideshows_lang'))
     # select.select_by_value('ja').click()
+    driver.execute_script('window.scrollTo(0, 10)')
     time.sleep(3) 
     driver.find_element_by_xpath("//select[@id='slideshows_lang']/option[@value='ja']").click()
     time.sleep(3) 
